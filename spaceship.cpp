a84de07fde85cc2263c0dacb687cd0a4590e93f8
esaslaga <esa.slaga@gmail.com>
2022-03-08 18:30:22 +0500

    fixed left and right

--- spaceship.cpp
+++ spaceship.cpp
@@ -1,65 +1,77 @@
 #include <cstdio>
 #include <iostream>
 #include <ncurses.h>
+#include <stdio.h>
 #include <string>
 #include <vector>
 using namespace std;
-vector<string> vec{"           \\\\\\_____", "        ###[==_____>",
-                   "           ///     "};
+vector<string> spaceship{"           \\\\\\_____", "        ###[==_____>",
+                         "           ///     "};
 vector<string> enemy{"    \\---/", "  _{ {0} }_", " {_________}"};
-auto move_space_ship(int &x, int &y, int key,int &check) {
+vector<string> stars{"   |", " -0- ", "  |", "* -0- -) *"};
+auto move_space_ship(int &x, int &y, int key) {
   if (key == 115) {
+    // move down
     y++;
     move(y, x);
     int Y = y;
-    for (int i = 0; i < vec.size(); i++) {
-      printw("%s\n", vec[i].c_str());
+    for (int i = 0; i < spaceship.size(); i++) {
+      printw("%s\n", spaceship[i].c_str());
+      Y++;
+      move(Y, x);
     }
   }
   if (key == 119) {
+    // move up
     y--;
     move(y, x);
     int Y = y;
-    for (int i = 0; i < vec.size(); i++) {
-      printw("%s\n", vec[i].c_str());
+    for (int i = 0; i < spaceship.size(); i++) {
+      printw("%s\n", spaceship[i].c_str());
+      Y++;
+      move(Y, x);
     }
   }
   if (key == 100) {
+    // move right
     x++;
     int Y = y;
     move(y, x);
-    for (int i = 0; i < vec.size(); i++) {
-      printw("%s\n", vec[i].c_str());
+    for (int i = 0; i < spaceship.size(); i++) {
+      printw("%s\n", spaceship[i].c_str());
       Y++;
       move(Y, x);
     }
   }
   if (key == 97) {
+    // move left
     x--;
     int Y = y;
     move(y, x);
-    for (int i = 0; i < vec.size(); i++) {
-      printw("%s\n", vec[i].c_str());
+    for (int i = 0; i < spaceship.size(); i++) {
+      printw("%s\n", spaceship[i].c_str());
       Y++;
       move(Y, x);
     }
   }
 }
 int main() {
+  printf("\033[33mred text\n");
   initscr();
+  COLOR_PAIR(5);
   auto x = 0;
   auto y = 0;
-  auto check = 0;
+  int helth_bar_lives = 3;
   move(y, x);
-  for (int i = 0; i < vec.size(); i++) {
-    printw("%s\n", vec[i].c_str());
+  for (int i = 0; i < spaceship.size(); i++) {
+    printw("%s\n", spaceship[i].c_str());
   }
   for (;;) {
-    move(0,0);
+    move(0, 0);
     int b = getch();
-    move(y,x);
+    move(y, x);
     clear();
-    move_space_ship(x, y, b,check);
+    move_space_ship(x, y, b);
     if (b == 27) {
       break;
     }
