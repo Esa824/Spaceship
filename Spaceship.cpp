439cc4e351c393a0c7cd4d4d7625408934fff8dc
esaslaga <esa.slaga@gmail.com>
2022-04-01 17:49:37 +0500

    just save whats done

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -1,6 +1,9 @@
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio/miniaudio.h"
 #include <cstdlib>
 #include <limits.h>
 #include <ncurses.h>
+#include <stdio.h>
 #include <string>
 #include <unistd.h>
 #include <unordered_map>
@@ -20,6 +23,13 @@ struct bullet {
   int check;
   bullet *next;
 };
+struct enemy_bullet {
+  int y;
+  int x;
+  int tick;
+  int check;
+  bullet *next;
+};
 struct star {
   int y = 0;
   int x = 0;
@@ -27,23 +37,27 @@ struct star {
 };
 vector<string> spaceship{"           \\\\\\_____", "        ###[==_____>",
                          "           ///     "};
-vector<string> number_of_spaces{"        ", "            ", "   "};
+vector<string> number_of_spaces_for_spaceship{"        ", "            ", "   "};
+vector<string> enemy{"  \\____/", "<_/__~0_\\", "(/_______\\)"};
+vector<string> number_of_spaces_for_alien_spaceship{"      ", "          ", "           "};
 void remove_space_ship(int &x, int &y) {
   int Y = y;
   int X = x;
   move(Y, X);
-  for (int i = 0; i < number_of_spaces.size(); i++) {
+  for (int i = 0; i < number_of_spaces_for_spaceship.size(); i++) {
     if (i == 1) {
       X -= 3;
     }
     if (i == 2) {
       X += 3;
     }
-    printw("%s\n", number_of_spaces[0].c_str());
+    printw("%s\n", number_of_spaces_for_spaceship[0].c_str());
     Y++;
     move(Y, X);
   }
   move(y, x);
+}
+void remove_alien_spaceship(int x){
 }
 void move_space_ship(int &x, int &y, int key) {
   if (key == 115) {
@@ -95,6 +109,11 @@ void move_space_ship(int &x, int &y, int key) {
     }
   }
 }
+void move_aline_space_ship(int y, int x, int getch){
+  if(getch == KEY_UP){
+    
+  }
+}
 void print_main_menu() {
   unordered_map<int, vector<int>> map;
   move(6, 70);
@@ -111,7 +130,7 @@ void print_main_menu() {
   printw("________");
   move(13, 84);
   printw("|      |");
-  move(14, 84);
+  move(14, 84); 
   printw("| play |");
   move(15, 84);
   printw("|______|");
@@ -123,18 +142,18 @@ void print_main_menu() {
   }
   clear();
 }
-void print_helth_bar() {
-  move(0, 0);
+void print_helth_bar(int x) {
+  move(0, x);
   printw("   _ _");
-  move(1, 0);
+  move(1, x);
   printw(" /  V  \\");
-  move(2, 0);
+  move(2, x);
   printw(" |     |");
-  move(3, 0);
+  move(3, x);
   printw("  \\   /");
-  move(4, 0);
+  move(4, x);
   printw("   \\ /");
-  move(5, 0);
+  move(5, x);
   printw("    *");
 }
 bullet *create_memory_for_bullet(int x, int y, int check) {
@@ -165,9 +184,10 @@ void shoot(bullet *sec_bullet_ptr) {
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
   initscr();
   noecho();
+  keypad(stdscr,true);
   print_main_menu();
   star *star_ptr = create_memory_for_star(myRand(5, 40), myRand(20, 200));
   star *sec_star_ptr = star_ptr;
@@ -178,6 +198,15 @@ int main() {
   star_ptr = sec_star_ptr;
   auto x = 0;
   auto y = 0;
+  auto y_enemy = myRand(5, 50);
+  auto x_enemy = myRand(100, 200);
+  move(y_enemy, x_enemy);
+  int Y = y_enemy;
+  for (int i = 0; i < enemy.size(); i++) {
+    printw("%s\n", enemy[i].c_str());
+    Y++;
+    move(Y, x_enemy);
+  }
   bullet *bullet_ptr = create_memory_for_bullet(0, 0, 0);
   bullet *sec_bullet_ptr = bullet_ptr;
   bullet *third_bullet_ptr = bullet_ptr;
